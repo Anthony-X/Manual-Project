bbcda485963e71d4533b3c230ffe52c94a0de741
Anthony <Anthony>
2016-01-30 18:19:17 +0300

    change the theme,add menu on toolbar (2acy)

--- app/src/main/java/com/tony/mymanual005/ContentActivity.java
+++ app/src/main/java/com/tony/mymanual005/ContentActivity.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
@@ -22,15 +25,17 @@ public class ContentActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_content);
+
         webView = (WebView) findViewById(R.id.webView);
 
         Intent intent = getIntent();
         //получаем строку и формируем имя ресурса
+
         res = intent.getIntExtra("head", 0);
         resName = "n" + intent.getIntExtra("head", 0);
         Log.i("name", resName);
+
         Context context = getBaseContext(); //получаем контекст
 
         //читаем текстовый файл из ресурсов по имени
@@ -38,13 +43,45 @@ public class ContentActivity extends ActionBarActivity {
 
         webView.loadDataWithBaseURL(null, text, "text/html", "en_US", null);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch (id){
+            case R.id.action_about_us:
+                Intent intent = new Intent(ContentActivity.this,AboutUsActivity.class);
+                startActivity(intent);
+                break;
+        }
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    // Обрабатываем переход на след/пред тему
     public void previousClick (View v){
         if(res-1>=0){
             res--;
         resName = "n" + res;
         String text = readRawTextFile(getBaseContext(), getResources().getIdentifier(resName, "raw", "com.tony.mymanual005"));
-
-        webView.loadDataWithBaseURL(null, text, "text/html", "en_US", null);}
+        webView.loadDataWithBaseURL(null, text, "text/html", "en_US", null);
+        }else if (res-1==-1){
+            onBackPressed();
+            /*Intent intent = new Intent(ContentActivity.this,MainActivity.class);
+            startActivity(intent);*/
+        }
     }
     public void nextClick (View v){
         if(res+1<=1){
@@ -75,4 +112,5 @@ public class ContentActivity extends ActionBarActivity {
         }
         return builder.toString();
     }
+
 }
