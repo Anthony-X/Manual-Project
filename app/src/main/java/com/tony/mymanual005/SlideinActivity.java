af1267acdc1b0a2bd5d0d2041d5e617d540b15ca
Андрей Зубехин < >
2016-02-04 18:01:28 +0300

    toolbar + start window

--- app/src/main/java/com/tony/mymanual005/SlideinActivity.java
+++ app/src/main/java/com/tony/mymanual005/SlideinActivity.java
@@ -1,5 +1,6 @@
 package com.tony.mymanual005;
 
+import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,7 +17,8 @@ import android.view.MenuItem;
 import com.tony.mymanual005.fragments.fragmentDicp;
 import com.tony.mymanual005.fragments.fragmentbm;
 
-public class SlideinActivity extends FragmentActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class SlideinActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     private fragmentDicp fragmentdicp = new fragmentDicp();
     private fragmentbm fragmentbm = new fragmentbm();
@@ -26,11 +29,8 @@ public class SlideinActivity extends FragmentActivity implements NavigationView.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slidein);
-
-
-         toolbar = (Toolbar) findViewById(R.id.toolbar);
-       /* setSupportActionBar(toolbar); Вызывает пустой метод*/
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,13 +40,43 @@ public class SlideinActivity extends FragmentActivity implements NavigationView.
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        FragmentManager fm = getFragmentManager();
+        fm.beginTransaction().replace(R.id.content_frame, new fragmentDicp()).commit();
     }
 
 
  /*   private void setSupportActionBar(Toolbar toolbar) {
     } я*/
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch (id){
+            case R.id.action_about_us:
+                Intent intent = new Intent(SlideinActivity.this,AboutUsActivity.class);
+                startActivity(intent);
+                break;
+        }
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, Prefs.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -57,48 +87,21 @@ public class SlideinActivity extends FragmentActivity implements NavigationView.
         }
     }
 
-    //Решить проблему с созданием меню ...
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        super.onCreateOptionsMenu(menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        switch (id){
-            case R.id.action_about_us:
-                Intent intent = new Intent(SlideinActivity.this,AboutUsActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.action_settings: break;
-                   }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+        FragmentManager fm = getFragmentManager();
         int id = item.getItemId();
-        FragmentTransaction ftrans = getFragmentManager().beginTransaction();
 
         if (id == R.id.nav_discipline) {
-            ftrans.replace(R.id.container,fragmentdicp);
-            toolbar.setTitle(R.string.nav_discipline);
+            fm.beginTransaction().replace(R.id.content_frame, new fragmentDicp()).commit();
         } else if (id == R.id.nav_bookmark) {
-            ftrans.replace(R.id.container, fragmentbm);
-            toolbar.setTitle(R.string.nav_bookmark);
+            fm.beginTransaction().replace(R.id.content_frame, new fragmentbm()).commit();
         }
-        ftrans.commit();
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
-    
 }
